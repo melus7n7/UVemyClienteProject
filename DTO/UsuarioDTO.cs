20e37e8842faf858d8c32c7d0405172b2b50721f
engaherr <kikgamboa@gmail.com>
2024-05-15 12:12:51 -0600

    feat(vistas/DTO): Agregar DTO de Usuario e inicio de sesión

---
+++ DTO/UsuarioDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace UVemyCliente.DTO
+{
+    public class UsuarioDTO
+    {
+        [JsonPropertyName("idUsuario")]
+        public int Id { get; set; }
+        [JsonPropertyName("nombres")]
+        public string Nombres { get; set; }
+        [JsonPropertyName("apellidos")]
+        public string Apellidos { get; set; }
+        [JsonPropertyName("correoElectronico")]
+        public string CorreoElectronico { get; set; }
+        [JsonPropertyName("contrasena")]
+        public string Contrasena { get; set; }
+        [JsonPropertyName("jwt")]
+        public string Token { get; set; }
+        [JsonPropertyName("detalles")]
+        public List<string> Detalles { get; set; }
+    }
+}
---
+++ Vistas/InicioSesionPagina.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UVemyCliente.Conexion;
+using UVemyCliente.DTO;
+using UVemyCliente.Utilidades;
+
+namespace UVemyCliente.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para InicioSesionPagina.xaml
+    /// </summary>
+    public partial class InicioSesionPagina : Page
+    {
+        private string _correoElectronico;
+        private string _contrasena;
+
+        public InicioSesionPagina()
+        {
+            InitializeComponent();
+        }
+
+        private void ClicIniciarSesion(object sender, RoutedEventArgs e)
+        {
+            txtBlockMensajeError.Text = string.Empty;
+            _correoElectronico = txtBoxCorreoElectronico.Text.Length == 0 ? string.Empty : txtBoxCorreoElectronico.Text;
+            _contrasena = pwdBoxContrasena.Password.Length == 0 ? string.Empty : pwdBoxContrasena.Password;
+
+            _ = IniciarSesionAsync();
+        }
+
+        private async Task IniciarSesionAsync()
+        {
+            UsuarioDTO credenciales = new UsuarioDTO
+            {
+                CorreoElectronico = _correoElectronico,
+                Contrasena = _contrasena
+            };
+
+            var json = JsonSerializer.Serialize(credenciales);
+            var contenido = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage respuestaHttp = await APIConexion.EnviarRequestAsync(HttpMethod.Post, "autenticacion", contenido);
+            int codigoRespuesta = (int)respuestaHttp.StatusCode;
+
+            if (codigoRespuesta == 200)
+            {
+                var jsonString = await respuestaHttp.Content.ReadAsStringAsync();
+                UsuarioDTO? usuarioVerificado = JsonSerializer.Deserialize<UsuarioDTO>(jsonString);
+                if (usuarioVerificado != null)
+                {
+                    SingletonUsuario.JWT = usuarioVerificado.Token;
+
+                    ExitoMensaje exitoMensaje = new ExitoMensaje();
+                    exitoMensaje.Show();
+                }
+            }
+            else
+            {
+                var jsonString = await respuestaHttp.Content.ReadAsStringAsync();
+                UsuarioDTO? errorJson = JsonSerializer.Deserialize<UsuarioDTO>(jsonString);
+
+                string[] detalles = errorJson?.Detalles.ToArray() ?? ["Error desconocido"];
+                string detallesConcatenados = string.Join(", ", detalles);
+                txtBlockMensajeError.Text = detallesConcatenados;
+
+                ErrorMensaje error = new ErrorMensaje();
+                error.Show();
+            }
+        }
+
+        private void ClicRegistrate(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
