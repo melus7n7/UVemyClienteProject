a76589e7b35aeb96d72335a3de3b2e54d600282a
sulem <sulem477@gmail.com>
2024-05-18 01:57:56 -0600

    feat: Estadisticas curso inicial

---
+++ DTO/EstadisticaCursoDTO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace UVemyCliente.DTO
+{
+    public class EstadisticaCursoDTO
+    {
+        [JsonPropertyName("nombre")]
+        public string NombreCurso { get; set; }
+        [JsonPropertyName("calificacionCurso")]
+        public double Calificacion { get; set; }
+        [JsonPropertyName("promedioComentarios")]
+        public double PromedioComentarios { get; set; }
+        [JsonPropertyName("estudiantesInscritos")]
+        public int EstudiantesInscritos { get; set; }
+        [JsonPropertyName("etiquetasCoinciden")]
+        public List<string> EtiquetasCoinciden { get; set; }
+        [JsonPropertyName("estudiantes")]
+        public List<string> EstudiantesCurso { get; set; }
+        [JsonPropertyName("clases")]
+        public List<ClaseEstadisticaDTO> ClasesEstadistcas { get; set; }
+    }
+
+    public class ClaseEstadisticaDTO
+    {
+        public int NumeroClase { get; set; }
+        [JsonPropertyName("nombre")]
+        public string Nombre { get; set; }
+        [JsonPropertyName("cantidadComentarios")]
+        public int CantidadComentarios { get; set; }
+    }
+
+}
--- Vistas/EstadisticasCursoPagina.xaml.cs
+++ Vistas/EstadisticasCursoPagina.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Reflection.Metadata;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +16,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UVemyCliente.Conexion;
+using UVemyCliente.DTO;
+using UVemyCliente.Utilidades;
 
 namespace UVemyCliente.Vistas
 {
@@ -20,9 +27,76 @@ namespace UVemyCliente.Vistas
     /// </summary>
     public partial class EstadisticasCurso : Page
     {
+        private EstadisticaCursoDTO _estadisticas;
         public EstadisticasCurso()
         {
             InitializeComponent();
+            
+            _ = RecuperarEstadisticasAsync(1);
+        }
+        private async Task RecuperarEstadisticasAsync(int idCurso)
+        {
+            string url = "cursos/estadisticas/" + idCurso;
+            HttpResponseMessage respuestaHttp = await APIConexion.EnviarRequestAsync(HttpMethod.Get, url);
+            int codigoRespuesta = (int)respuestaHttp.StatusCode;
+
+            if (codigoRespuesta >= 400)
+            {
+                Debug.WriteLine(codigoRespuesta);
+                ErrorMensaje error = new ErrorMensaje("Ocurrió un error y no se pudo recuperar las estadísticas, inténtelo más tarde");
+                error.Show();
+            }
+            else
+            {
+                var jsonString = await respuestaHttp.Content.ReadAsStringAsync();
+                EstadisticaCursoDTO? estadisticas = JsonSerializer.Deserialize<EstadisticaCursoDTO>(jsonString);
+
+                if (estadisticas != null)
+                {
+                    _estadisticas = estadisticas;
+                    MostrarDatos();
+                }
+                
+            }
+
+        }
+
+        private void MostrarDatos()
+        {
+            txtBlockNombreCurso.Text = _estadisticas.NombreCurso;
+            txtBlockCalificacionTotal.Text = _estadisticas.Calificacion.ToString();
+            txtBlockPromedioComentarios.Text = _estadisticas.PromedioComentarios.ToString();
+            txtBlockEstudiantesTotales.Text = _estadisticas.EstudiantesInscritos.ToString();
+
+            if (_estadisticas.EtiquetasCoinciden != null)
+            {
+                string etiquetas = "";
+                for (int i = 0; i < _estadisticas.EtiquetasCoinciden.Count; i++)
+                {
+                    etiquetas = (i == 0) ? _estadisticas.EtiquetasCoinciden[i] : etiquetas + ", " + _estadisticas.EtiquetasCoinciden[i];
+                }
+                txtBlockEtiquetas.Text = etiquetas;
+            }
+
+            if (_estadisticas.EstudiantesCurso != null)
+            {
+                lstBoxEstudiantes.ItemsSource = _estadisticas.EstudiantesCurso;
+            }
+
+            if (_estadisticas.ClasesEstadistcas != null)
+            {
+                for(int i=0; i<_estadisticas.ClasesEstadistcas.Count; i++)
+                {
+                    _estadisticas.ClasesEstadistcas[i].NumeroClase = i + 1;
+                }
+                lstBoxClases.ItemsSource = _estadisticas.ClasesEstadistcas;
+            }
+
+        }
+
+        private void ClicRegresar(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
         }
     }
 }
