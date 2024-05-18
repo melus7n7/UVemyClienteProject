d55ad627262f701430f9d0252b6e8ee2aa5b2c69
Enrique Gamboa Hernández <113001293+engaherr@users.noreply.github.com>
2024-05-18 15:26:35 -0600

    pr(master): Agregar registro de usuario accediendo desde inicio de sesión y con validaciones en cliente (#1)
    * fix(InicioSesionPagina.xaml): remover estilos no utilizados
    
    * feat(InicioSesion): Agregar validaciones a campos correo y contrasena con utiilidad creada
    
    * feat(recursos): añadir iconos para FormularioUsuarioPagina
    
    * feat(DTO/Vistas): Añadir registro de perfil nuevo hasta envío de correo (pendiente confirmar para registrar) y navegabilidad
    
    * feat: Añadir registro de usuario con validaciones de código de verificación

---
+++ DTO/EtiquetaDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace UVemyCliente.DTO
+{
+    public class EtiquetaDTO
+    {
+        [JsonPropertyName("idEtiqueta")]
+        public int IdEtiqueta { get; set; }
+        [JsonPropertyName("nombre")]
+        public required string Nombre { get; set; }
+    }
+}
---
+++ Vistas/CodigoVerificacionPagina.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UVemyCliente.Conexion;
+using UVemyCliente.DTO;
+using UVemyCliente.Utilidades;
+
+namespace UVemyCliente.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para CodigoVerificacionPagina.xaml
+    /// </summary>
+    public partial class CodigoVerificacionPagina : Page
+    {
+        private UsuarioDTO _usuario;
+
+        public CodigoVerificacionPagina(UsuarioDTO usuario)
+        {
+            InitializeComponent();
+            _usuario = usuario;
+        }
+
+        private void ClicConfirmar(object sender, RoutedEventArgs e)
+        {
+            if (ValidarCampo())
+            {
+                _usuario.CodigoVerificacion = txtCodigoVerificacion.Text;
+                _ = VerificarCodigoAsync();
+            }
+        }
+
+        private bool ValidarCampo()
+        {
+            txtBlockMensajeError.Text = string.Empty;
+            bool esValido = true;
+
+            if (string.IsNullOrEmpty(txtCodigoVerificacion.Text) || txtCodigoVerificacion.Text.Length < 4)
+            {
+                txtBlockMensajeError.Text = "El código de verificación debe tener 4 dígitos.";
+                esValido = false;
+            }
+            
+            return esValido;
+        }
+
+        private async Task VerificarCodigoAsync()
+        {
+            var json = JsonSerializer.Serialize(_usuario);
+            var contenido = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage respuestaHttp = await APIConexion.EnviarRequestAsync(HttpMethod.Post, "autenticacion/registro", contenido);
+            if (respuestaHttp.IsSuccessStatusCode)
+            {
+                ExitoMensaje exitoMensaje = new();
+                exitoMensaje.Show();
+
+                InicioSesionPagina inicioSesionPagina = new();
+                this.NavigationService?.Navigate(inicioSesionPagina);
+                this.NavigationService?.RemoveBackEntry();
+                SingletonUsuario.JWT = string.Empty;
+            }
+            else
+            {
+                var jsonString = await respuestaHttp.Content.ReadAsStringAsync();
+                UsuarioDTO? errorJson = JsonSerializer.Deserialize<UsuarioDTO>(jsonString);
+
+                string[] detalles = errorJson?.Detalles.ToArray() ?? ["Error desconocido"];
+                string detallesConcatenados = string.Join(", ", detalles);
+                txtBlockMensajeError.Text = detallesConcatenados + ". Verifique la información e intentélo de nuevo más tarde";
+            }
+        }
+
+        private void VerificarEntradaNumérica(object sender, TextCompositionEventArgs e)
+        {
+            if (!EsNumerico(e.Text) || e.Text.Contains(" "))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private static bool EsNumerico(string texto)
+        {
+            return int.TryParse(texto, out _);
+        }
+
+        private void ClicRegresar(object sender, RoutedEventArgs e)
+        {
+            InicioSesionPagina inicioSesionPagina = new();
+            this.NavigationService?.Navigate(inicioSesionPagina);
+        }
+    }
+}
