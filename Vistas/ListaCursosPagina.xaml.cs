b9eaa938a1489eb43c0600b54915ee63039e09f8
JJacobMS <montielsalasjesus@gmail.com>
2024-05-24 21:12:02 -0600

    feat (Consultar cursos): Se obtienen los cursos con sus imagenes y se termina de crear la pantalla de lista cursos

--- Vistas/ListaCursosPagina.xaml.cs
+++ Vistas/ListaCursosPagina.xaml.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +18,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UVemyCliente.Conexion;
+using UVemyCliente.DTO;
+using UVemyCliente.Utilidades;
 
 namespace UVemyCliente.Vistas
 {
@@ -21,11 +30,93 @@ namespace UVemyCliente.Vistas
     public partial class ListaCursosPagina : Page
     {
         private List<CheckBox> _checkBoxes;
+        private ObservableCollection<EtiquetaDTO> _etiquetas = new ObservableCollection<EtiquetaDTO>();
+        private ObservableCollection<TiposCursos> _tiposCursos = new ObservableCollection<TiposCursos>();
+        private List<CursoLista> _listaCursos;
+        private int pagina = 0;
         public ListaCursosPagina()
         {
+            pagina = 0;
             InitializeComponent();
             _checkBoxes = new List<CheckBox> { checkBox1, checkBox2, checkBox3, checkBox4 };
+            _tiposCursos = new ObservableCollection<TiposCursos>
+            {
+                new TiposCursos
+                {
+                    IdTipoCurso = 1,
+                    Nombre = "Cursos creados"
+                },
+                new TiposCursos
+                {
+                    IdTipoCurso = 2,
+                    Nombre = "Cursos inscritos"
+                },
+            };
+            CargarComboboxTipoCurso();
+            _ = CargarEtiquetasAsync();
         }
+
+        private void CargarComboboxTipoCurso()
+        {
+            cmbBoxTipoCurso.Items.Clear();
+            cmbBoxTipoCurso.ItemsSource = _tiposCursos;
+        }
+
+        private async Task CargarEtiquetasAsync()
+        {
+            HttpResponseMessage respuestaHttp = await APIConexion.EnviarRequestAsync(HttpMethod.Get, "etiquetas");
+            if (respuestaHttp.IsSuccessStatusCode)
+            {
+                var json = await respuestaHttp.Content.ReadAsStringAsync();
+                _etiquetas = System.Text.Json.JsonSerializer.Deserialize<ObservableCollection<EtiquetaDTO>>(json) ?? [];
+                cmbBoxEtiquetaCurso.ItemsSource = _etiquetas;
+                Debug.WriteLine(json);
+                _ = CargarCursosPagina(pagina);
+            }
+            else
+            {
+                ErrorMensaje errorMensaje = new("Error. No se pudo conectar con el servidor. Inténtelo de nuevo o hágalo más tarde.");
+                errorMensaje.Show();
+            }
+        }
+
+        private async Task CargarCursosPagina(int pagina)
+        {
+            HttpResponseMessage respuestaHttp = await APIConexion.EnviarRequestAsync(HttpMethod.Get, "cursoslistas/"+pagina);
+            Debug.WriteLine(respuestaHttp);
+            if (respuestaHttp.IsSuccessStatusCode)
+            {
+                var json = await respuestaHttp.Content.ReadAsStringAsync();
+                Debug.WriteLine(json);
+                List<CursoListBox> cursos = JsonConvert.DeserializeObject<List<CursoListBox>>(json);
+                Debug.WriteLine(cursos);
+                Debug.WriteLine(cursos.Count);
+                int registrosPorFila = 3;
+
+                for (int f = 0; f < (cursos.Count / registrosPorFila) - 1; f++)
+                {
+                    for (int i = 0; i < registrosPorFila; i++)
+                    {
+                        _listaCursos = new List<CursoLista>()
+                        {
+                            new CursoLista()
+                            {
+
+                            }
+                        };
+                    }
+                }
+
+                lstBoxCreditos.ItemsSource = _listaCursos;
+            }
+            else
+            {
+                ErrorMensaje errorMensaje = new("Error. No se pudo conectar con el servidor. Inténtelo de nuevo o hágalo más tarde.");
+                errorMensaje.Show();
+            }
+        }
+
+
 
         private void ClicRegresar(object sender, RoutedEventArgs e)
         {
@@ -61,6 +152,47 @@ namespace UVemyCliente.Vistas
         {
             FormularioCursoPagina curso = new FormularioCursoPagina();
             this.NavigationService.Navigate(curso);
+        }
+
+        public class CursoListBox
+        {
+            [JsonProperty("idCurso")]
+            public int IdCurso { get; set; }
+            [JsonProperty("titulo")]
+            public string Titulo { get; set; }
+            [JsonProperty("documentos")]
+            public List<DocumentoListBox> Documento { get; set; }
+        }
+
+        public class DocumentoListBox
+        {
+            [JsonProperty("idDocumento")]
+            public int IdDocumento { get; set; }
+            [JsonProperty("archivo")]
+            public ArchivoListBox Archivo { get; set; }
+        }
+
+        public class ArchivoListBox
+        {
+            [JsonProperty("type")]
+            public string Type { get; set; }
+
+            [JsonProperty("data")]
+            public byte[] Data { get; set; }
+        }
+
+        public class CursoLista
+        {
+            public int idCurso { get; set; }
+            public byte[] Archivo { get; set; }
+            public string Titulo { get; set; }
+            public int idCurso2 { get; set; }
+            public byte[] Archivo2 { get; set; }
+            public string Titulo2 { get; set; }
+            public int idCurso3 { get; set; }
+            public byte[] Archivo3 { get; set; }
+            public string Titulo3 { get; set; }
+
         }
     }
 }
